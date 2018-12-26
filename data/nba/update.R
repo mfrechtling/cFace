6449d24897e68c73510e98f48eb65a2e1d6e9f26
Michael <michael@frechtling.net>
2018-12-27 01:06:17 +1100

    Finished update to nba update.r

--- data/nba/update.R
+++ data/nba/update.R
@@ -2,19 +2,28 @@ updateScores <- function()
 {
         raw <- read.csv("./data/nba/raw_scores.csv", header=TRUE)
         scores <- data.frame(raw$Home, raw$Home.Score, raw$Visitor, raw$Visitor.Score, rep(0, length(raw$Home)), rep(0, length(raw$Home)))
-        teams <- unique(scores$Home)
-        elo <- rep(1300, length(teams))
-        elo <- data.frame(teams, elo)
         colnames(scores) <- c("Home", "Score", "Away", "Score", "Elo.Home", "Elo.Away")
+        teams <- as.character(unique(scores$Home))
+        elo_vec <- as.numeric(rep(1300, length(teams)))
+        elo <- data.frame(teams, elo_vec)
+        print(elo)
         for (i in 1:nrow(scores))
         {
+            print(i)
+            home <- scores$Home[i]
+            away <- scores$Away[i]
+
+            print(home)
+            print(away)
+
             scores$Elo.Home[i] <- elo[scores$Home[i], 2]
             scores$Elo.Away[i] <- elo[scores$Away[i], 2]
-            elo_new <- elo_update(scores[i, 2], scores[i, 4], elo[scores$Home[i], 2], elo[scores$Away[i], 2])
-            elo[scores$Home[i], 2] <- elo_new[1]
-            elo[scores$Away[i], 2] <- elo_new[2]
+            elo_new <- update_elo(scores[i, 2], scores[i, 4], elo[scores$Home[i], 2], elo[scores$Away[i], 2])
+            print(elo_new)
+            elo[as.character(scores$Home[i]), 2] <- elo_new[1]
+            elo[as.charactre(scores$Away[i]), 2] <- elo_new[2]
         }
-        write.table(scores, "./data/nba/scores.csv", row.names = FALSE, col.names = TRUE, sep=",")
+        #write.table(scores, "./data/nba/scores.csv", row.names = FALSE, col.names = TRUE, sep=",")
 }
 
 updateElo <- function()
@@ -29,4 +38,5 @@ update_elo <- function(home_score, away_score, elo_home, elo_away)
     e_a <- r_a / (r_h + r_a)
     rp_h <- r_h + (30*(home_score - e_h))
     rp_a <- r_a + (30*(away_score - e_a))
+    return(c(rp_h, rp_a))
 }
